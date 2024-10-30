8d1dc94e1809f0af18530e4bdf79bd58075a571c
Tausif148 <sheikhtousif148@gmail.com>
2024-10-30 17:36:18 +0530

    changes

--- script.js
+++ script.js
@@ -1,85 +1,79 @@
-// const inputBox = document.querySelector('.input-box');
-// const searchBtn = document.getElementById('searchBtn');
-// const weather_img = document.querySelector('.weather-img');
-// const temperature = document.querySelector('.temperature');
-// const description = document.querySelector('.description');
-// const humidity = document.getElementById('humidity');
-// const wind_speed = document.getElementById('wind-speed');
+//  Geting value from 
+async function fetchWeatherData(cityValue) {
+    try {
+        const weatherData = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&units=metric&appid=54d3fc2327c271abc85608d4ea4c015f`);
+        let result = await weatherData.json()
+        console.log(result);
+        updateOnUI(result);
 
-// const location_not_found = document.querySelector('.location-not-found');
-
-// const weather_body = document.querySelector('.weather-body');
-
-
-
-// async function checkWeather(city) {
-//     const api_key = "4cd0eee81294c867b4bc4cfc64e998c5";
-//     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`;
-
-//     const weather_data = await fetch(`${url}`).then(response => response.json());
-
-
-//     if (weather_data.cod === `404`) {
-//         location_not_found.style.display = "flex";
-//         weather_body.style.display = "none";
-
-//         console.log("error");
-//         return;
-//     }
-
-//     console.log("run");
-
-//     location_not_found.style.display = "none";
-
-//     weather_body.style.display = "flex";
-//     temperature.innerHTML = `${Math.round(weather_data.main.temp - 273.15)}°C`;
-//     description.innerHTML = `${weather_data.weather[0].description}`;
-
-//     humidity.innerHTML = `${weather_data.main.humidity}%`;
-//     wind_speed.innerHTML = `${weather_data.wind.speed}Km/H`;
+        weather_body.style = "display:flex";
+        location_not_found.style = "display:none"
+    } catch (error) {
+        console.log(error);
+        location_not_found.style = "display:block";
+        city_blank.textContent = "Sorry, Location not found!!!";
+        city_blank.style = "display:block";
+        weather_body.style = "display:none";
+    }
+}
 
 
-//     switch (weather_data.weather[0].main) {
-//         case 'Clouds':
-//             weather_img.src = "/assets/cloud.png";
-//             break;
-//         case 'Clear':
-//             weather_img.src = "/assets/clear.png";
-//             break;
-//         case 'Rain':
-//             weather_img.src = "/assets/rain.png";
-//             break;
-//         case 'Mist':
-//             weather_img.src = "/assets/mist.png";
-//             break;
-//         case 'Snow':
-//             weather_img.src = "/assets/snow.png";
-//             break;
+// Set value on UI
+const cityName = document.querySelector('.cityName');
+const temperature = document.querySelector('.temperature');
+const description = document.querySelector('.description');
+const humidity = document.querySelector('#humidity');
+const wind_speed = document.querySelector('#wind-speed');
+const date = document.querySelector('.date');
 
-//     }
-
-//     console.log(weather_data);
-// }
+const weather_image = document.querySelector('.weather-image');
 
 
-// searchBtn.addEventListener('click', () => {
-//     checkWeather(inputBox.value);
+function updateOnUI(result) {
+    cityName.textContent = result.name;
+    temperature.innerHTML = result.main.temp + '<sup>°C</sup>';
+    description.textContent = result.weather[0].main;
+    humidity.textContent = `${result.main.humidity}%`;
+    wind_speed.textContent = `${result.wind.speed} Km/H`;
+
+    // set weather img 
+
+    const inImgFormate = result.weather[0].main.toLocaleLowerCase();
+
+    console.log(inImgFormate);
+    // console.log(typeof inImgFormate)
+
+    weather_image.src = `img/${inImgFormate}.png`;
+
+    const currentDate = new Date();
+    date.textContent = currentDate.toDateString();
+}
 
 
-// });
+// get value as per search by user
+const search_city = document.querySelector('.search-city');
+const city = document.querySelector('.city');
 
-// const cityName = document.querySelector('.input-box').value;
+const weather_body = document.querySelector('.weather-body');
+const location_not_found = document.querySelector('.location-not-found');
+const city_blank = document.querySelector('.city-blank');
 
-searchBtn.addEventListener('click', function getValue() {
-    const cityName = document.querySelector('.input-box').value;
-    console.log(cityName);
-
-})
-
-
+// fetchWeatherData("pune");
 
 
-const a = 'https://api.openweathermap.org/data/2.5/weather?q=nagpur&appid=54d3fc2327c271abc85608d4ea4c015f';
+search_city.addEventListener('submit', (e) => {
+    e.preventDefault();
 
+    const cityValue = city.value;
+    // console.log(cityValue);
+    if (cityValue !== "") {
+        fetchWeatherData(cityValue);
+        city.value = "";
+    } else {
+        location_not_found.style = "display:block";
+        city_blank.style = "display:block";
+        weather_body.style = "display:none";
 
-console.log(a); 
+        console.log("Please enter a city name.");
+    }
+});
